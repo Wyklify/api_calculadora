e1ee527d60bb2ab036dd6aa01a514ad374a13325
Wyklify Jhonson <wyklifyjhonson01@gmail.com>
2026-09-12 15:37:46 -0300

    Bug de conexão corrigido

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 
-app.use(cors());
+
 
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const swaggerDocument = {
     openapi: '3.0.0',
